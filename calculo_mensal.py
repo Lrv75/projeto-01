a5f4f539bda8a712860b8550d39fe8b5b8396f77
LUCIANO RIBEIRO VIANA <100737911+Lrv75@users.noreply.github.com>
2023-01-04 10:33:27 -0300

    Add files via upload

---
+++ calculo_mensal.py
@@ -0,0 +1,10 @@
+
+n=int(input('aluguel '))
+l=int(input('Luz  '))
+a=int(input('Água  ' ))
+i=int(input('Internet '))
+f=int(input('Faculdade '))
+c=int(input('Compra '))
+C=int(input('Celular '))
+Total=n+l
+print('Total do fechamento do mês foi de , {:.2f} R$.' . format(Total,n,l,a,i,f,c,C))
